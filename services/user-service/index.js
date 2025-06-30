f92a8c78bf18bc4ab0febbeafec3551d3bf0b604
tanishaaraoo07 <damalatanisha7@gmail.com>
2025-06-30 22:19:57 +0530

    fix: added function.json and index.js

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -7,3 +7,10 @@ exports.handler = async (event) => {
     }),
   };
 };
+
+module.exports = async function (context, req) {
+  context.res = {
+    status: 200,
+    body: "✅ Hello from Azure Function!"
+  };
+};
