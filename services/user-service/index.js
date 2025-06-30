3c5ae595754e10229ac0752433f013e31604e91c
tanishaaraoo07 <damalatanisha7@gmail.com>
2025-06-30 22:24:04 +0530

    fix: add function.json and update Node.js runtime

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -1,16 +1,7 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "✅ User service is running!",
-      input: event
-    }),
-  };
-};
-
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
     body: "✅ Hello from Azure Function!"
   };
 };
+
